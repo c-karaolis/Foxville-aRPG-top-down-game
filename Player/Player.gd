85dcf810a323f2f82e29ebcad42a53558c5700ec
Constantinos Karaolis <kostaskaraolis@gmail.com>
2021-01-10 15:56:23 +0200

    p4 Created y-sorting world, changed player scene pivot

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,7 @@ const MAX_SPEED = 80
 const FRICTION = 500
 
 var velocity = Vector2.ZERO
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
