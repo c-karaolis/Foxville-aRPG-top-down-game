b48dcd4120fdef57bbb5d3924dea9ad08c28e615
Constantinos Karaolis <kostaskaraolis@gmail.com>
2021-01-14 10:23:21 +0200

    Created state machine for player actions

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,17 +4,35 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+enum{
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
-#Below was replaced by onready before variable
-#func _ready():
-#	animationPlayer = $AnimationPlayer
+
+func _ready():
+	animationTree.active = true
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+			
+	
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -29,3 +47,9 @@ func _physics_process(delta):
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	pass
