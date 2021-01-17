42baf1dc1f20210693c08421b37c4f57371615b6
Constantinos Karaolis <kostaskaraolis@gmail.com>
2021-01-17 17:49:05 +0200

    p14 enemy stats, exports, custom signals, setters/getters

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
-const ACCELERATION = 500
-const MAX_SPEED = 80
-const FRICTION = 500
-const ROLL_SPEED = 100
+export var ACCELERATION = 500
+export var MAX_SPEED = 80
+export var FRICTION = 500
+export var ROLL_SPEED = 100
 enum{
 	MOVE,
 	ROLL,
