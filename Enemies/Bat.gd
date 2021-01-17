25dcf24d7d6c627e25393061348b0c132dc6ef37
Constantinos Karaolis <kostaskaraolis@gmail.com>
2021-01-17 18:32:20 +0200

    removed bat health debugs

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -5,11 +5,6 @@ var knockback = Vector2.ZERO
 
 onready var stats = $Stats
 
-func _ready():
-	print(stats.max_health)
-	print(stats.health)
-
-
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
